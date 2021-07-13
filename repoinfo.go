6ea9af66e18550c922ce9ed1143cfe09362d3a1b
Webster <webster19950528@gmail.com>
2021-07-13 17:29:32 +0800

    Code refactoring.

--- repoinfo.go
+++ repoinfo.go
@@ -12,13 +12,18 @@ import(
 	"github.com/buger/jsonparser"
 )
 
-var opts struct {
-	Single string `short:"s" long:"single" description:"Show single repo info."`
-	Author string `short:"a" long:"author"`
-	Repo int `short:"r" long:"repo" description:"Repo num to check from top."`
-}
+var (
+	fork, steal, count, repo, max int
+	m map[string]int = map[string]int{"": 0,}
+	author string
+	opts struct {
+		Single string `short:"s" long:"single" description:"Show single repo info."`
+		Author string `short:"a" long:"author"`
+		Repo int `short:"r" long:"repo" description:"Repo num to check from top."`
+	}
+)
 
-func IsForkRepo(site string)bool {
+func GetBody(site string)[]byte{
 	res, err := http.Get(site)
 	if err != nil {
 		log.Fatal(err)
@@ -29,8 +34,10 @@ func IsForkRepo(site string)bool {
 	if err != nil {
 		log.Fatal(err)
 	}
-	reader := bytes.NewReader(body)
-
+	return body
+}
+func IsForkRepo(site string)bool {
+	reader := bytes.NewReader(GetBody(site))
 	doc, err := goquery.NewDocumentFromReader(reader)
 	if err != nil {
 		log.Fatal(err)
@@ -44,58 +51,55 @@ func IsForkRepo(site string)bool {
 	fmt.Println("The repo is not fork.")
 	return false
 }
+
+func ParseAuthorInfo(){
+	for num := 1; num <= max; num++{
+		site := fmt.Sprintf("https://api.github.com/users/%s/repos?per_page=100&page=%d", author, num)
+		body := GetBody(site)
+		for i := 0; i < 100; i++{
+			isfork, err := jsonparser.GetBoolean(body,fmt.Sprintf("[%d]",i),"fork")
+			if err == nil {
+				ctime, _ := jsonparser.GetString(body,fmt.Sprintf("[%d]",i),"created_at")
+				utime, _ := jsonparser.GetString(body,fmt.Sprintf("[%d]",i),"updated_at")
+				codetype, _ := jsonparser.GetString(body,fmt.Sprintf("[%d]",i),"language")
+				if isfork {
+					fork++
+					if ctime == utime {steal++}
+				}else{
+					m[codetype]++
+				}
+				count++
+			}
+		}
+	}
+}
+
+func PrintInfo(){
+	fmt.Println("Total repo from",author,":",count)
+	fmt.Println("|____Fork repo:", fork)
+	fmt.Println("|    |____Non modified fork:", steal)
+	fmt.Println("|____Non fork repo:", count - fork)
+	fmt.Println("     |____Noncode repo:",m[""])
+	for key, value := range m{
+		if key != "" && value != 0{
+			fmt.Println("         ",key,"repo:",value)
+		}
+	}
+}
+
 func main() {
-	var fork, steal, count, repo, max int
-	var m map[string]int = map[string]int{"": 0,}
-	var author string
 
 	_,err := flags.ParseArgs(&opts, os.Args)
 	if err != nil {
 		panic(err)
 	}
-	if opts.Single != ""{
+	if opts.Single != "" {
 		IsForkRepo(opts.Single)
-	}
-	if opts.Author != "" && opts.Repo != 0 {
+	} else if opts.Author != "" && opts.Repo != 0 {
 		author = opts.Author
 		repo = opts.Repo
 		max = repo/100 + 1
-		for num := 1; num <= max; num++{
-			res, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s/repos?per_page=100&page=%d", author, num))
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer res.Body.Close()
-
-			body, err := ioutil.ReadAll(res.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-			for i := 0; i < 100; i++{
-				isfork, err := jsonparser.GetBoolean(body,fmt.Sprintf("[%d]",i),"fork")
-				if err == nil {
-					ctime, _ := jsonparser.GetString(body,fmt.Sprintf("[%d]",i),"created_at")
-					utime, _ := jsonparser.GetString(body,fmt.Sprintf("[%d]",i),"updated_at")
-					codetype, _ := jsonparser.GetString(body,fmt.Sprintf("[%d]",i),"language")
-					if isfork {
-						fork++
-						if ctime == utime {steal++}
-					}else{
-						m[codetype]++
-					}
-					count++
-				}
-			}
-		}
-		fmt.Println("Total repo from",author,":",count)
-		fmt.Println("|____Fork repo:", fork)
-		fmt.Println("|    |____Non modified fork:", steal)
-		fmt.Println("|____Non fork repo:", count - fork)
-		fmt.Println("     |____Noncode repo:",m[""])
-		for key, value := range m{
-			if key != "" && value != 0{
-				fmt.Println("         ",key,"repo:",value)
-			}
-		}
+		ParseAuthorInfo()
+		PrintInfo()
 	}
 }
